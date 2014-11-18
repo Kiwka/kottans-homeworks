7a9e5eb258aa1de9dc9f9918e795bb5b29ff88ea
Olena Sovyn <olenasovyn@gmail.com>
2014-11-18 18:19:22 +0200

    Tasks in the H-6 up to Basic: reduce done

--- Homework-6/program.js
+++ Homework-6/program.js
@@ -1,31 +1,80 @@
 function upperCaser(input) {
+	'use strict';
 	return input.toUpperCase();
-};
+}
 
 function repeat(operation, num) {
+	'use strict';
 	var i;
 	for (i=0; i<num; i++) {
 		operation();
 	}
-};
+}
 
 function doubleAll(numbers) {
+	'use strict';
 	return numbers.map(function(x) {
-		return x*2
+		return x*2;
 	});
-};
+}
 
 function getShortMessages(messages) {
+	'use strict';
 	var result = [];
 	messages.filter(function(mes){
 		return mes.message.length < 50;
 	}).map(function(mes){
 		result.push(mes.message);
-	})
+	});
 	return result;
-};
+}
+
+function checkUsersValid(goodUsers) {
+	'use strict';
+	return function(submittedUsers) {
+		return submittedUsers.every(function(submittedUser){
+			return goodUsers.some(function(goodUser){
+				return submittedUser.id == goodUser.id;
+			});
+		});
+	};
+}
+
+function countWords(inputWords) {
+	'use strict';
+	return inputWords.reduce(function(acc, curr){
+		if (typeof acc[curr] == 'undefined') {
+			acc[curr] = 1;
+		} else {
+			acc[curr] += 1;
+		}
+		return acc;
+	}, {});
+}
+
+function reduce(arr, fn, initial) {
+	'use strict';
+	if(!arr.length) return;
+	initial = fn(initial, arr[0], 0, arr);
+	reduce(arr.slice(1), fn, initial);
+	return initial;
+}
+
+function duckCount() {
+	'use strict';
+	arguments.map(function(curr){
+		if (curr.hasOwnProperty('duck')) {
+			arguments.splice(arguments.indexOf(curr),1);
+		}
+	});
+	return arguments.length;
+}
 
 module.exports = upperCaser;
 module.exports = repeat;
 module.exports = doubleAll;
 module.exports = getShortMessages;
+module.exports = checkUsersValid;
+module.exports = countWords;
+module.exports = reduce;
+module.exports = duckCount;
